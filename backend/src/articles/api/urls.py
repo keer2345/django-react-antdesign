25ff6b4fc80755357b761c8a9cfc65b460b7e6c9
keer2345 <keer2345@gmail.com>
2018-09-16 22:49:40 +0800

    ViewSets

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,12 +1,20 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from .views import (ArticleCreateView, ArticleDeleteView, ArticleDetailView,
-                    ArticleListView, ArticleUpdateView)
+from .views import ArticleViewSet
 
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('create/', ArticleCreateView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
-    path('<pk>/update', ArticleUpdateView.as_view()),
-    path('<pk>/delete', ArticleDeleteView.as_view()),
-]
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, base_name='articles')
+urlpatterns = router.urls
+
+# from django.urls import path
+#
+# from .views import (ArticleCreateView, ArticleDeleteView, ArticleDetailView,
+#                     ArticleListView, ArticleUpdateView)
+#
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleCreateView.as_view()),
+#     path('<pk>', ArticleDetailView.as_view()),
+#     path('<pk>/update', ArticleUpdateView.as_view()),
+#     path('<pk>/delete', ArticleDeleteView.as_view()),
+# ]
--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,32 +1,38 @@
-from rest_framework.generics import (CreateAPIView, DestroyAPIView,
-                                     ListAPIView, RetrieveAPIView,
-                                     UpdateAPIView)
+# from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+#                                      ListAPIView, RetrieveAPIView,
+#                                      UpdateAPIView)
+
+from rest_framework import viewsets
 
 from articles.models import Article
 
 from .serializes import ArticleSerializer
 
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+class ArticleViewSet (viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
-
-
-class ArticleDetailView(RetrieveAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
 
-
-class ArticleCreateView(CreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleUpdateView(UpdateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleDeleteView(DestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
