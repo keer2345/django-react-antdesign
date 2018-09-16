a195fccdff997686c544f34e01a2be13d5d7519e
keer2345 <keer2345@gmail.com>
2018-09-16 14:29:39 +0800

    backend

---
+++ backend/src/articles/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ArticleListView,ArticleDetailView
+
+urlpatterns = [
+    path('',ArticleListView.as_view()),
+    path('<pk>',ArticleDetailView.as_view()),
+]
---
+++ backend/src/articles/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework.generics import ListAPIView,RetrieveAPIView
+
+from articles.models import Article
+from .serializes import ArticleSerializer
+
+
+class ArticleListView(ListAPIView):
+    queryset=Article.objects.all()
+    serializer_class=ArticleSerializer
+
+class ArticleDetailView(RetrieveAPIView):
+    queryset=Article.objects.all()
+    serializer_class=ArticleSerializer
