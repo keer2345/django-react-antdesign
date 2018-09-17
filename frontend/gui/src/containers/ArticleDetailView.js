0c870706b7b3754d97ad4ff327cc4c2188b44a07
keer2345 <keer2345@gmail.com>
2018-09-17 21:58:51 +0800

    form

--- frontend/gui/src/containers/ArticleDetailView.js
+++ frontend/gui/src/containers/ArticleDetailView.js
@@ -1,7 +1,9 @@
-import { Card } from "antd";
+import { Button,Card } from "antd";
 import axios from "axios";
 import React from "react";
 import Articles from "../components/Article";
+
+import CustomForm from "../components/Form";
 
 class ArticleDetail extends React.Component {
   state = {
@@ -16,11 +18,26 @@ class ArticleDetail extends React.Component {
     });
   }
 
+  handleDelete = (event) => {
+    const articleID = this.props.match.params.articleID;
+    axios;delete(`http://127.0.0.1:8000/api/${articleID}/`);
+    this.props.history.push('/');
+    this.forceUpdate();
+  }
+
   render() {
     return (
+      <div>
       <Card title={this.state.article.title}>
         <p>{this.state.article.content}</p>
       </Card>
+        <br/>
+        <h2>Update this article</h2>
+      <CustomForm requestType="put" articleID={this.state.article.id} btnText="Update"/>
+      <form onSubmit={this.handleDelete}>
+          <Button type="danger" htmlType="submit">Delete</Button>
+      </form>
+      </div>
     );
   }
 }
--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React from "react";
 import Articles from "../components/Article";
+import CustomForm from "../components/Form";
 
 class ArticleList extends React.Component {
   state = {
@@ -16,7 +17,14 @@ class ArticleList extends React.Component {
   }
 
   render() {
-    return <Articles data={this.state.articles} />;
+    return (
+      <div>
+        <Articles data={this.state.articles} />
+        <br/>
+        <h2>Create an article</h2>
+        <CustomForm requestType="post" articleID={null} btnText="Create"/>
+      </div>
+    )
   }
 }
 
